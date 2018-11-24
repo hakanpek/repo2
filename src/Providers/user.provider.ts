799a5654dea5907ca6107f70c60c04258a4f6aa8
hakanpek <32062772+hakanpek@users.noreply.github.com>
2018-11-24 21:40:18 +0300

    ..

--- src/Providers/user.provider.ts
+++ src/Providers/user.provider.ts
@@ -6,8 +6,8 @@ import { Http, Response} from '@angular/http';
 
 @Injectable()
  export class UserService{
-     private _url: string = 'https://front-end-test.azurewebsites.net/api/todo';
-    private baseUrl: string = 'https://nodebackendtest.herokuapp.com/products/all';
+    private _url: string = 'https://nodebackendtest.herokuapp.com/products/all';
+    private baseUrl: string = 'https://nodebackendtest.herokuapp.com/products/create';
 
     constructor(private _http: Http){
     }

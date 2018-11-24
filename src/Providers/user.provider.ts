140ac54650d510977b3f5b9a8e3fc0ca2a064836
hakanpek <32062772+hakanpek@users.noreply.github.com>
2018-11-24 21:52:17 +0300

    ..

--- src/Providers/user.provider.ts
+++ src/Providers/user.provider.ts
@@ -9,6 +9,23 @@ import { Http, Headers, RequestOptions, Response, Jsonp} from '@angular/http';
     private _url: string = 'https://nodebackendtest.herokuapp.com/products/all';
     private baseUrl: string = 'https://nodebackendtest.herokuapp.com/products/create';
 
+    private extractData(res: Response) {
+      let body = res.json();
+      return body || {};
+  }
+
+  private handleError(error: Response | any) {
+    // In a real world app, we might use a remote logging infrastructure
+    //let errMsg: string;
+    //if (error instanceof Response) {
+    //    const body = error.json() || '';
+    //    const err = body.error || JSON.stringify(body);
+    //    errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    //} else {
+    //    errMsg = error.message ? error.message : error.toString();
+    //}
+
+}
     constructor(private _http: Http){
     }
 
@@ -26,7 +43,8 @@ import { Http, Headers, RequestOptions, Response, Jsonp} from '@angular/http';
       let options = new RequestOptions({ headers: headers });
        var data = `{ Name: "${name}", Color:"${price}"}`;
        return this._http.post(`${this.baseUrl}`, data, this.options)
-      .map((res: Response) => res.json());
+       .map(this.extractData)
+       .catch(this.handleError);
   }
 
   }

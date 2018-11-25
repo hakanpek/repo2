d37a9c204c0960127b52c7604cc90ede7f5d8df7
hakanpek <32062772+hakanpek@users.noreply.github.com>
2018-11-25 11:19:16 +0300

    ..

--- src/Providers/todo-data.service.ts
+++ src/Providers/todo-data.service.ts
@@ -46,7 +46,7 @@ import { Observable } from 'rxjs/Observable';
 
     /////////   DELETE
     removeTodo(todo: Todo) {
-    const deleteUrl = this._url  + todo.name;
+    const deleteUrl = this._url  + todo.id;
      return this._http.delete(deleteUrl)
      .map((response: Response) => response.json())
       }
